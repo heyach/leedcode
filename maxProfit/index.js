51598b2f1a395b1a0c30bb0b7713871d69d5083b
chengjiajia <chengjiajia@haocang.com>
2022-03-08 14:20:03 +0800

    卖出股票的最佳时机

--- maxProfit/index.js
+++ maxProfit/index.js
@@ -2,18 +2,18 @@ var maxProfit = function(prices) {
     if(!prices.length) {
         return 0
     }
-    var h = prices[0]
-    var r = 0
+    var min = prices[0]
+    var max = 0
     for(let i = 1;i < prices.length;i++) {
-        if(prices[i] - h > 0) {
+        if(prices[i] - min > 0) {
             // 有利润了，更新最大利润，比如h更新到1的时候，遍历到5，利润为4，遍历到3的时候利润为2，取最大值
-            r = Math.max(r, prices[i] - h)
+            max = Math.max(max, prices[i] - min)
         } else {
             // 更新最小值，遍历到1的时候，h=1，遍历到3的时候还是1
-            h = Math.min(h, prices[i])
+            min = Math.min(min, prices[i])
         }
     }
-    return r
+    return max
 };
 maxProfit([7,1,5,3,6,4])
 maxProfit([7,6,4,3,1])
