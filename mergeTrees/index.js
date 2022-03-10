083590ea07a3d32589541ea1f6b6f9e9b27238f0
chengjiajia <chengjiajia@haocang.com>
2022-03-10 17:29:27 +0800

    合并二叉树

--- mergeTrees/index.js
+++ mergeTrees/index.js
@@ -1,4 +1,11 @@
 
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+var root = new TreeNode(1, new TreeNode(3, new TreeNode(5, null, null), null), new TreeNode(2, new TreeNode(4, null, null)))
+var root2 = new TreeNode(2, new TreeNode(4, new TreeNode(6, null, null), null), new TreeNode(2, new TreeNode(4, null, null)))
 var mergeTrees = function(root1, root2) {
     if(root1 == null) {
         return root2
@@ -12,3 +19,4 @@ var mergeTrees = function(root1, root2) {
 
     return newRoot
 };
+mergeTrees(root, root2)
